985fc12fd376cd0b8d3037e880a5da063d753982
Taras Nytiaha <taras.nytiaha@gmail.com>
2023-10-26 23:16:02 +0200

    feat: added tests

---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from catalog.models import Author
+
+
+class AuthorModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        Author.objects.create(first_name="XXX", last_name="Bob")
+
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field("first_name").verbose_name
+        self.assertEquals(field_label, "first name")
+
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field("date_of_death").verbose_name
+        self.assertEquals(field_label, "died")
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field("first_name").max_length
+        self.assertEquals(max_length, 100)
